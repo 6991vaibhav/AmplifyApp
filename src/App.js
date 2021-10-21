9e87f8b60dd585c5a60f8713c68a13da11d60c72
Vaibhav Jain <vaibhavjain@Vaibhavs-MacBook-Pro.local>
2021-10-21 12:55:48 +0530

    Changed Home page message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello!!! Continuous deployment is working...
         </a>
       </header>
     </div>
